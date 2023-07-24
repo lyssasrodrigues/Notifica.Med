22976dbca5b5138267f5b7fd3cd94d9dbfbf8e03
Lyssa <lyssa.rodrigues@upe.br>
2023-07-24 18:25:32 -0300

    base do site

---
+++ src/Home/index.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+
+
+
+function Home() {
+  return (
+    <>
+    <p>
+    <h1>Sistema de Notificação de Interações Medicamentosas</h1>
+    </p>
+
+    <p>
+    <input type="text" placeholder= "Digite o nome do medicamento"/>
+    </p>
+    <p>
+<button type= "button">Adicionar</button>
+</p>
+    </>
+ 
+ 
+ )
+}
+
+export default Home
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './Home.jsx'
-
+import Home from './Home/index.jsx'
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
